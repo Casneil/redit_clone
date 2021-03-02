9646c822d68b6f7245a8363e952c8110d918b9cc
casneil <casneil.simpson@gmail.com>
2021-03-02 10:46:12 +0100

    wip(routes): login register routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,2 +1,23 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
+import express from "express";
+import morgan from "morgan";
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+	return res.send("Hello World");
+});
+
+app.listen(5000, async () => {
+	console.log("Running at http:localhost:5000");
+
+	try {
+		await createConnection();
+		console.log("database connected");
+	} catch (error) {
+		console.log(error);
+	}
+});
