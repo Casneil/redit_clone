bc8d5960716f73a9d3ebfa5a11516ce88b70890d
casneil <casneil.simpson@gmail.com>
2021-03-04 13:27:46 +0100

    wip(single post): working on single post

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -31,6 +31,8 @@ const PostPage = () => {
 
   const handleVote = async (value: number) => {
     if (!authenticated) router.push("/login");
+    //If vote is the same reset vote
+    if (value === post.userVote) value = 0;
     try {
       const res = await Axios.post("/misc/vote", {
         identifier,
@@ -72,7 +74,7 @@ const PostPage = () => {
             {post && (
               <div className="flex">
                 {/* Vote section */}
-                <div className="w-10 py-3 text-center bg-gray-200 rounded-l">
+                <div className="w-10 py-3 text-center rounded-l">
                   {/* Upvote */}
                   <div
                     className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
@@ -96,6 +98,25 @@ const PostPage = () => {
                       })}
                     ></i>
                   </div>
+                </div>
+                <div className="p-2">
+                  <div className="flex items-center">
+                    <p className="text-xs text-gray-500">
+                      Posted by
+                      <Link href={post.username}>
+                        <a className="mx-1 hover:underline">{post.username}</a>
+                      </Link>
+                      <Link href={post.url}>
+                        <a className="mx-1 hover:underline">
+                          {dayjs(post.createdAt).fromNow()}
+                        </a>
+                      </Link>
+                    </p>
+                  </div>
+                  {/* Post title */}
+                  <h1 className="my-1 text-xl font-medium">{post.title}</h1>
+                  {/* Post body */}
+                  <p className="my-3 text-sm">{post.body}</p>
                 </div>
               </div>
             )}
